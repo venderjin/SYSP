390a34d88a37b6b9040ed6362e195cf45feaf522
venderjin <90211945+venderjin@users.noreply.github.com>
2022-06-22 19:53:15 +0900

    Create setbuf.c

---
+++ setbuf.c
@@ -0,0 +1,21 @@
+#include <
+stdio.h >
+main()
+{
+printf
+안녕하세요 , "); sleep(
+printf
+리눅스입니다 !"); sleep(
+printf
+(" n"); sleep(1);
+setbuf
+stdout ,
+printf
+여러분 , "); sleep(
+printf
+반갑습니다 "); sleep(
+printf
+(" ^^"); sleep(
+printf
+(" n"); sleep(1);
+}
