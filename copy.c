cbae3f7a22f75ddc8139f72e2580e6e1078623a6
venderjin <90211945+venderjin@users.noreply.github.com>
2022-06-22 19:24:48 +0900

    Create copy.c

---
+++ copy.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+#include "copy.h"
+/* copy: from을 to 에 복사 ; to 가 충분히 크다고 가정*/
+void copy(char from[], char to[])
+{
+  int i;
+  i=0;
+  while ((to[i] = i ]) != \0')
+    ++i;
+}
