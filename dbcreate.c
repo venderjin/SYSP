992739c9dbbab74af2ac824675c9edf49e4211d3
venderjin <90211945+venderjin@users.noreply.github.com>
2022-06-22 19:35:20 +0900

    Update dbcreate.c

--- dbcreate.c
+++ dbcreate.c
@@ -12,3 +12,17 @@ int main( int argc , char argv[])
         fprintf(stderr , 사용법 : %s file n", argv [0])
         exit(1);
 }
+
+               
+if ((fd = argv [1], O_WRONLY|O_CREAT|O_EXCL, 0640)) == -1) {
+        perror(argv [1]);
+        exit(2);
+}
+printf("9s % 8s % 4s n", " 학번 ", 이름 ", 점수");
+while (scanf ("%d %s %d", &record.id, record.name, record.score ) == 3) {
+        lseek(fd , (record.id START_ID) * sizeof (record), SEEK_
+        write(fd , (char *) &record, sizeof (record)
+}
+close(fd);
+exit(0);
+}
